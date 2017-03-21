07814734125fe722e0145a3052ce4242d7054574
Isaque Dias <isaquediasm@gmail.com>
2017-03-21 16:30:10 +0100

    change identation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,60 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import profile from '../resources/profile.png';
 
 const domain = 'https://abroadwith.imgix.net';
 
 class Card extends Component {
-		/**
+  /**
 		* Render host's info like picture.
 		*
 		* @param {string} hostPhoto
 		*/
-		renderHostInfo(hostPhoto = profile) {
-			return (
-				<div className="host">
-					<img className="photo" src={ hostPhoto } />
-				</div>
-			);
-		}
+  renderHostInfo(hostPhoto = profile) {
+    return (
+      <div className="host">
+        <img className="photo" src={hostPhoto}/>
+      </div>
+    );
+  }
 
-		/**
+  /**
 		* Render the room's picture based on the passed parameter. If the parameter is undefined,
 		* catch the first home picture.
 		*
 		* @param {string} roomPhoto
 		*/
-		renderRoomPhoto(roomPhoto = this.props.data.homePhotosWithOrder[0].split(',')[0]) {
-			const style = {
-					'backgroundImage': `url(${domain + roomPhoto})`,
-					'backgroundSize': 'cover',
-					'backgroundPosition': '50% 50%'
-			};
+  renderRoomPhoto(roomPhoto = this.props.data.homePhotosWithOrder[0].split(',')[0]) {
+    const style = {
+      'backgroundImage': `url(${domain + roomPhoto})`,
+      'backgroundSize': 'cover',
+      'backgroundPosition': '50% 50%'
+    };
 
-			return <div className="image" style={style}></div>;
-		}
+    return <div className="image" style={style}></div>;
+  }
 
-    render() {
-				let {
-					roomPhoto,
-					hostPhoto,
-					hostName,
-					homeCity
-				} = this.props.data;
+  render() {
+    let {roomPhoto, hostPhoto, hostName, homeCity} = this.props.data;
 
-        return(
-					<div className="card">
-						{this.renderRoomPhoto(roomPhoto)}
-						{this.renderHostInfo(hostPhoto && `${domain + hostPhoto}`)}
+    return (
+      <div className="card">
+        {this.renderRoomPhoto(roomPhoto)}
+        {this.renderHostInfo(hostPhoto && `${domain + hostPhoto}`)}
 
-						<div className="info">
-							<h4 className="title">{ `${hostName}'s Home` }</h4>
-							<p className="place">{ homeCity }</p>
-							<p className="place">ITA, ENG</p>
-						</div>
+        <div className="info">
+          <h4 className="title">{`${hostName}'s Home` } </h4>
+					<p className="place">{ homeCity }</p > <p className="place">ITA, ENG</p> </div>
 					</div>
-        );
-    }
+    );
+  }
 }
 
 export default Card;
-/**/
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,23 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Card from './Card';
 
 class CardList extends Component {
-	render() {
-		this.props.items.results.map(item => {
-			if(item.hostName === 'DragonflyHill') {
-				console.log(item);
-			}
-		});
-
-		return(
-			<div>
-				{
-					this.props.items.results
-						.map((item, idx) => <Card key={idx} data={item}/>)
-				}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        {
+          this.props.items.results
+            .map((item, idx) => <Card key={idx} data={item}/>)
+        }
+      </div>
+    );
+  }
 }
 
 export default CardList;
