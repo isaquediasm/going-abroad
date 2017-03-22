74d872748c29e4b53a9ff64a1caa033f1e186766
Isaque Dias <isaquediasm@gmail.com>
2017-03-22 22:02:42 +0100

    ident

--- src/components/Card.js
+++ src/components/Card.js
@@ -43,13 +43,20 @@ class Card extends Component {
     };
     const userCurrency = availableCurrencies[this.props.currency];
 
-    return <div className="price">{ userCurrency + price.toFixed(2) }</div>
+    return <div className="price">{userCurrency + price.toFixed(2)}</div>
   }
 
   render() {
-    let { roomPhoto, hostPhoto, hostName, homeCity, price, interestedLanguages } = this.props.data;
-    const languages = interestedLanguages && interestedLanguages
-      .map((lang, idx) => <span key={idx}>{lang}</span>);
+    let {
+      roomPhoto,
+      hostPhoto,
+      hostName,
+      homeCity,
+      price,
+      interestedLanguages
+    } = this.props.data;
+
+    const languages = interestedLanguages && interestedLanguages.map((lang, idx) => <span key={idx}>{lang}</span>);
 
     return (
       <div className="card">
@@ -59,7 +66,7 @@ class Card extends Component {
 
         <div className="info">
           <h4 className="title">{`${hostName}'s Home`}</h4>
-					<p className="place">{homeCity}</p >
+					<p className="place">{homeCity}</p>
           <p className="languages">{languages}</p>
         </div>
 			</div>
